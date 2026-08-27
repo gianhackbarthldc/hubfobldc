73bb6378b1d94769f7e0bdbc04360889730bda10
Gian Hackbarth <gian.hackbarth@ldc.com>
2026-08-27 16:06:41 -0300

    ci: fix caminhos vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Em dev: base '/' para servir local. Em build/prod: '/hufobldc/' para GitHub Pages.
-  base: process.env.NODE_ENV === 'production' ? '/hufobldc/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/hubfobldc/' : '/',
 })
